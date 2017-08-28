2393fc2088b80a83a85274080b9b2073b4bc164f
Matthias L. Jugel <leo@thinkberg.com>
2017-08-28 09:58:35 +0200

    dump plain, add button a press to toggle hex dump

--- source/CalliopeDataLogger.cpp
+++ source/CalliopeDataLogger.cpp
@@ -19,7 +19,7 @@ MicroBit uBit;
 
 void hexdump(const char *prefix, const uint8_t *b, int size) {
     for (int i = 0; i < size; i += 16) {
-        if (prefix && strlen(prefix) > 0) printf("%s %06x: ", prefix, i);
+        if (prefix != NULL && strlen(prefix) > 0) printf("%s %06x: ", prefix, i);
         for (int j = 0; j < 16; j++) {
             if ((i + j) < size) printf("%02x", b[i + j]); else printf("  ");
             if ((j + 1) % 2 == 0) putchar(' ');
@@ -32,26 +32,40 @@ void hexdump(const char *prefix, const uint8_t *b, int size) {
     }
 }
 
+static bool hex = false;
+
+void toggleHex(MicroBitEvent event) {
+    (void) event;
+    hex = !hex;
+}
+
 void onData(MicroBitEvent event) {
     (void) event;
     uBit.display.image.setPixelValue(2, 2, 255);
     PacketBuffer packet = uBit.radio.datagram.recv();
-    hexdump("DATA", packet.getBytes(), packet.length());
+    if (hex) {
+        hexdump("DATA", packet.getBytes(), packet.length());
+    } else {
+        for (int i = 0; i < packet.length(); i++) putchar(packet.getByte(i));
+    }
 }
 
 int main(void) {
     uBit.init();
+    uBit.serial.setTxBufferSize(255);
     uBit.serial.baud(115200);
     uBit.serial.send("Calliope Data Logger v1.0\r\n");
-
+    MicroBitSerialMode ::
     uBit.radio.enable();
     uBit.radio.setGroup(0b01100110);
     uBit.messageBus.listen(MICROBIT_ID_RADIO, MICROBIT_RADIO_EVT_DATAGRAM, onData);
 
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, toggleHex);
+
     uBit.display.setDisplayMode(DISPLAY_MODE_GREYSCALE);
-    while(1) {
+    while (1) {
         uBit.sleep(50);
-        int v = uBit.display.image.getPixelValue(2,2);
-        if(v > 0) uBit.display.image.setPixelValue(2, 2, max(0, v - 20));
+        int v = uBit.display.image.getPixelValue(2, 2);
+        if (v > 0) uBit.display.image.setPixelValue(2, 2, max(0, v - 20));
     }
 }
